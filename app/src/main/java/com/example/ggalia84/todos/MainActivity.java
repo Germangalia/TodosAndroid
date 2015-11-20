96449181909d395df9e186965c9ba9793d45f82d
Germangalia <germangalia@iesebre.com>
2015-11-20 17:32:47 +0100

    Classe CustomAdapter

--- app/src/main/java/com/example/ggalia84/todos/MainActivity.java
+++ app/src/main/java/com/example/ggalia84/todos/MainActivity.java
@@ -16,6 +16,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -26,18 +27,10 @@ import java.lang.reflect.Type;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static final String SHARED_PREFERENCES_TODOS = "SP_TODOS";
-    private static final String TODO_LIST = "todo_list";
-
+    private static final String SHARED_PREFERENCES_TODOS ="SP_TODOS";
+    private static final String TODO_LIST ="todo_list" ;
     private Gson gson;
-
     public TodoArrayList tasks;
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,67 +41,62 @@ public class MainActivity extends AppCompatActivity
         SharedPreferences todos = getSharedPreferences(SHARED_PREFERENCES_TODOS, 0);
         String todoList = todos.getString(TODO_LIST, null);
 
-        if (todoList == null){
-            String initial_json = "{{\"name\":\"Comprar llet\", \"done\": true, \"priority\": 2},\n" +
-                    "        {\"name\":\"Comprar pa\", \"done\": true, \"priority\": 1},\n" +
-                    "        {\"name2\":\"Fer exercici\", \"done\": false, \"priority\": 3}}";
+        /* JSON Example
+        [
+            {name:"Compra llet", "done": true, "priority": 2},
+            {name:"Compra pa", "done": true, "priority": 1},
+            {name:"Fer exercici", "done": false, "priority": 3}
+        ]
+         */
 
+        if (todoList == null){
+            String initial_json= "[{name:\"Compra llet\", \"done\": true, \"priority\": 2},\n" +
+                    "            {name:\"Compra pa\", \"done\": true, \"priority\": 1},\n" +
+                    "            {name:\"Fer exercici\", \"done\": false, \"priority\": 3}]";
             SharedPreferences.Editor editor = todos.edit();
             editor.putString(TODO_LIST, initial_json);
             editor.commit();
             todoList = todos.getString(TODO_LIST, null);
-        }else {
-            //TODO
         }
 
-
-
-        Log.d("TAG_PROVA", "*********************************************************************************************");
+        Log.d("TAG_PROVA", "******************************************************************");
         Log.d("TAG_PROVA", todoList);
-        Log.d("TAG_PROVA", "*********************************************************************************************");
+        Log.d("TAG_PROVA", "******************************************************************");
 
-        Toast.makeText(this, todoList, Toast.LENGTH_LONG).show();
+//        Toast.makeText(this, todoList, Toast.LENGTH_LONG).show();
 
-        /* JSON EXAMPLE
-
+        /* JSON Example
         [
-
-        {"name":"Comprar llet", "done": true, "priority": 2},
-        {"name":"Comprar pa", "done": true, "priority": 1},
-        {"name2":"Fer exercici", "done": false, "priority": 3}
-
+            {name:"Compra llet", "done": true, "priority": 2},
+            {name:"Compra pa", "done": true, "priority": 1},
+            {name:"Fer exercici", "done": false, "priority": 3}
         ]
-
          */
 
-        Type arrayTodoList = new TypeToken<TodoArrayList>() {}.getType();
+        Type arrayTodoList = new TypeToken<TodoArrayList>(){}.getType();
         this.gson = new Gson();
         TodoArrayList temp = gson.fromJson(todoList, arrayTodoList);
 
         if (temp != null){
             tasks = temp;
-        }else {
-            //ERROR TODO
-        }
 
+        } else {
+            //Error TODO
+        }
 
         ListView todoslv = (ListView) findViewById(R.id.todoListView);
 
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-        startActivity(intent);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class );
+                startActivity(intent);
                 //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
+                //      .setAction("Action", null).show();
             }
         });
 
